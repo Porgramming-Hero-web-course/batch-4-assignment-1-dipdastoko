2964fe92f97d78415332603ee21916c8b4526baf
rpamgi <dipdastoko@gmail.com>
2024-11-21 14:09:23 +0600

    problem-8 solved

---
+++ problem-8.ts
@@ -0,0 +1,14 @@
+{
+    function validateKeys<T>(obj: T, keys: (keyof T)[]) {
+        let keyExists = true;
+        for (const key of keys) {
+            if (typeof obj === "object" && obj && obj[key]===undefined) {
+                keyExists = false;
+            } 
+        }
+        return keyExists;
+        
+}
+const person = { name: "Alice", age: 25, email: "alice@example.com" };
+console.log(validateKeys(person, ["name", "address"]));
+}
